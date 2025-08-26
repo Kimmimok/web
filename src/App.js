85e8d23e37a6de35f0726a951d59f51209e782f9
kimmimok-7 <kjh@hyojacho.es.kr>
2025-08-27 08:55:16 +0900

    08-27-0855

--- src/App.js
+++ src/App.js
@@ -9,15 +9,23 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          {/* 모바일 예약폼이 기본 루트 */}
-          <Route 
-            path="/" 
-            element={<GoogleSheetInput formData={formData} setFormData={setFormData} />} 
-          />
-          {/* 다른 모든 경로를 루트로 리다이렉트 */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <header className="app-header">
+          <div className="header-inner">
+            <h1 className="brand">스테이 하롱 트레블</h1>
+          </div>
+        </header>
+
+        <main className="app-main">
+          <Routes>
+            {/* 모바일 예약폼이 기본 루트 */}
+            <Route 
+              path="/" 
+              element={<GoogleSheetInput formData={formData} setFormData={setFormData} />} 
+            />
+            {/* 다른 모든 경로를 루트로 리다이렉트 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
